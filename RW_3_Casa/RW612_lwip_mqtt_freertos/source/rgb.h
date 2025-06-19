1bebc5a1b35e5e0ea621c8020faef8e756bfc855
Alejandro Ramírez <114745147+Alekei1@users.noreply.github.com>
2025-06-19 11:08:08 -0600

    Header del driver del led RGB

---
+++ RW_3_Casa/RW612_lwip_mqtt_freertos/source/rgb.h
@@ -0,0 +1,18 @@
+/*
+ * rgb.h
+ *
+ *  Created on: 19 jun. 2025
+ *      Author: armzr
+ */
+
+#ifndef RGB_H_
+#define RGB_H_
+
+#include <stdio.h>
+#include "pin_mux.h"
+#include "fsl_gpio.h"
+#include "board.h"
+
+void init_RGB(void);
+
+#endif /* RGB_H_ */
