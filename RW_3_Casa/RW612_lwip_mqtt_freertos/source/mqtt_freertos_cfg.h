3659bccc1d130fc4d800dc8f0dfe9f7f73b67d3f
Alejandro Ramírez <114745147+Alekei1@users.noreply.github.com>
2025-06-19 11:24:30 -0600

    Config para mqtt

---
+++ RW_3_Casa/RW612_lwip_mqtt_freertos/source/mqtt_freertos_cfg.h
@@ -0,0 +1,21 @@
+/*
+ * mqtt_freertos_cfg.h
+ *
+ *  Created on: 19 jun. 2025
+ *      Author: armzr
+ */
+
+#ifndef MQTT_FREERTOS_CFG_H_
+#define MQTT_FREERTOS_CFG_H_
+
+/*! @brief MQTT server host name or IP address. */
+#ifndef EXAMPLE_MQTT_SERVER_HOST
+#define EXAMPLE_MQTT_SERVER_HOST "broker.hivemq.com"
+#endif
+
+/*! @brief MQTT server port number. */
+#ifndef EXAMPLE_MQTT_SERVER_PORT
+#define EXAMPLE_MQTT_SERVER_PORT 1883
+#endif
+
+#endif /* MQTT_FREERTOS_CFG_H_ */
