bf31ffb9cfaf38916102a6657bcb76da3cb4f7f3
Alejandro Ramírez <114745147+Alekei1@users.noreply.github.com>
2025-06-19 11:25:14 -0600

    Separar los elementos de config

--- RW_3_Casa/RW612_lwip_mqtt_freertos/source/mqtt_freertos.c
+++ RW_3_Casa/RW612_lwip_mqtt_freertos/source/mqtt_freertos.c
@@ -26,16 +26,6 @@
  * Definitions
  ******************************************************************************/
 
-/*! @brief MQTT server host name or IP address. */
-#ifndef EXAMPLE_MQTT_SERVER_HOST
-#define EXAMPLE_MQTT_SERVER_HOST "broker.hivemq.com"
-#endif
-
-/*! @brief MQTT server port number. */
-#ifndef EXAMPLE_MQTT_SERVER_PORT
-#define EXAMPLE_MQTT_SERVER_PORT 1883
-#endif
-
 /*! @brief Stack size of the temporary lwIP initialization thread. */
 #define INIT_THREAD_STACKSIZE 1024
 
