376674797cccd35c62b9c83f7c7456df327c774f
Oscar <olunadlm@outlook.com>
2025-06-19 23:38:43 -0600

    Se corrigio algo del main

--- K66_2_Cuarto/REDES_HANDS_ON_HELLO_MQTT_lwip_mqtt_freertos/source/main.c
+++ K66_2_Cuarto/REDES_HANDS_ON_HELLO_MQTT_lwip_mqtt_freertos/source/main.c
@@ -7,21 +7,21 @@
 #include "clock_config.h"
 #include "board.h"
 
-void prueba(){
-
-//	char data[5]= {'a', 'y', 'u', 'd', 'a'};
-	char data[10]= {'m', 'o', 'v', 'i', 'm', 'i', 'e', 'n', 't', 'o'};
-//	char data[3]= {'g', 'a', 's'};
-    char dato[32]= "";
-
-    for(int i=0; i<10; i++){
-    	char temp[2] = {data[i], '\0'};
-        strcat(dato, temp);
-    }
-//    data_received("ayuda");
-	data_received(dato);
-    vTaskDelete(NULL);
-}
+//void prueba(){
+//
+////	char data[5]= {'a', 'y', 'u', 'd', 'a'};
+//	char data[10]= {'m', 'o', 'v', 'i', 'm', 'i', 'e', 'n', 't', 'o'};
+////	char data[3]= {'g', 'a', 's'};
+//    char dato[32]= "";
+//
+//    for(int i=0; i<10; i++){
+//    	char temp[2] = {data[i], '\0'};
+//        strcat(dato, temp);
+//    }
+////    data_received("ayuda");
+//	data_received(dato);
+//    vTaskDelete(NULL);
+//}
 
 int main(void)
 {
